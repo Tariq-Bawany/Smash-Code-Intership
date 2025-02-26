ba67ddc1cb0d4179b3cc815d87f15f65c8472c03
Muhammad Tariq <muhammad.tariq.19.03.2005@gmail.com>
2025-02-26 17:56:51 +0500

    updated JS task-2

--- JS Task-2 (To Do App)/main.js
+++ JS Task-2 (To Do App)/main.js
@@ -31,11 +31,20 @@ function renderTodo() {
     todoArr.forEach((task, i) => {
         todoList.innerHTML += `
         <li>
-            <span>${task}</span>
-            <span>
-                <i class="fa-solid fa-pen" id="edit-btn-${i}"></i>
-                <i class="fa-solid fa-trash" id="delete-btn-${i}"></i>
-            </span>
+            <div class="edit-display" id="edit-display-${i}">
+                <input type="text" placeholder="Enter New task to edit |">
+                    <span>
+                        <i class="fa-solid fa-check" id="confirm-btn-${i}"></i>
+                        <i class="fa-solid fa-close" id="close-btn-${i}"></i>
+                    </span>
+            </div>
+            <div class="task-display" id="task-display-${i}">
+                <span>${task}</span>
+                <span>
+                    <i class="fa-solid fa-pen" id="edit-btn-${i}"></i>
+                    <i class="fa-solid fa-trash" id="delete-btn-${i}"></i>
+                </span>
+            </div>
         </li>`
 
     })
@@ -63,22 +72,49 @@ function editDeleteEventDelegation(event) {
         let btnIdCount = btnId.lastIndexOf('-')
         let btnIdIndex = btnId.slice(btnIdCount + 1)
 
+        // confirm edit task btn
+        let confirmBtn = document.getElementById(`confirm-btn-${btnIdIndex}`)
+        // confirm edit task btn
+        let closeBtn = document.getElementById(`close-btn-${btnIdIndex}`)
+
+        // editDisplay = the element of the selected task
+        let editDisplay =document.getElementById(`edit-display-${btnIdIndex}`)
+
+        // taskDisplay = the element of the selected task
+        let taskDisplay =document.getElementById(`task-display-${btnIdIndex}`)
+
         if (event.target.id.includes('edit-btn')) {
-            let newTask = prompt('Enter Your to edit your Task!')
-            if (!newTask == "") {
-                if (!todoArr.includes(newTask)) {
-                    todoArr[btnIdIndex] = newTask
-                    renderTodo()
+            
+            
+            taskDisplay.style.display = "none"
+            editDisplay.style.display = "flex"
+
+            // input inside the edit display
+            let editInput = document.querySelector(`#edit-display-${btnIdIndex} input` )
+            confirmBtn.addEventListener("click", ()=>{
+                let newTask = editInput.value
+                if (!newTask == "") {
+                    if (!todoArr.includes(newTask)) {
+                        todoArr[btnIdIndex] = newTask
+                        renderTodo()
+                        editDisplay.style.display = "none"
+                        taskDisplay.style.display = "flex"
+                    } else {
+                        // console.log('this value is already in the task');
+                        modal("this value is already in the task")
+    
+                    }
                 } else {
-                    // console.log('this value is already in the task');
-                    modal("this value is already in the task")
-
+                    // console.log('please add task!');
+                    modal("please add task!")
+    
                 }
-            } else {
-                // console.log('please add task!');
-                modal("please add task!")
-
-            }
+            })
+            closeBtn.addEventListener("click",()=>{
+                taskDisplay.style.display = "flex"
+            editDisplay.style.display = "none"
+            })
+            // let newTask = "hellow"
         } else if (event.target.id.includes('delete-btn')) {
             let confirmDelete = confirm("Do you want to delete this task")
             if (confirmDelete) {
